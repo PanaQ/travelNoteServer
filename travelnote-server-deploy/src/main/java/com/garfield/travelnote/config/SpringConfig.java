de4a6ceb33738eea70db4228c1ce9400ded5e4e1
朱士竹 <zhushizhu@zhexinit.com>
2018-02-22 12:01:01 +0800

    添加datasource和redis配置

--- travelnote-server-deploy/src/main/java/com/garfield/travelnote/config/SpringConfig.java
+++ travelnote-server-deploy/src/main/java/com/garfield/travelnote/config/SpringConfig.java
@@ -6,12 +6,11 @@ import com.zaxxer.hikari.HikariDataSource;
 import org.apache.ibatis.plugin.Interceptor;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
-import org.springframework.data.redis.core.HashOperations;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.ValueOperations;
 
 /**
  * Mybatis Spring 配置类
@@ -24,12 +23,12 @@ import org.springframework.data.redis.core.ValueOperations;
 public class SpringConfig {
 
     @Bean
-    public HikariDataSource hikariDataSource() {
+    public HikariDataSource hikariDataSource(DataSourceProperties dataSourceProperties) {
         HikariConfig config = new HikariConfig();
-        config.setJdbcUrl("jdbc:mysql://localhost:3306/travel_note?characterEncoding=utf-8&amp;useSSL=false");
-        config.setDriverClassName("com.mysql.jdbc.Driver");
-        config.setUsername("root");
-        config.setPassword("123456");
+        config.setJdbcUrl(dataSourceProperties.getUrl());
+        config.setDriverClassName(dataSourceProperties.getDriverClassName());
+        config.setUsername(dataSourceProperties.getUsername());
+        config.setPassword(dataSourceProperties.getPassword());
         //连接池中允许的最大连接数。缺省值：10；推荐的公式：((core_count * 2) + effective_spindle_count)
         config.setMaximumPoolSize(10);
         //等待连接池分配连接的最大时长（毫秒）
@@ -39,11 +38,7 @@ public class SpringConfig {
         //一个连接的生命时长（毫秒），超时而且没被使用则被释放（retired），缺省:30分钟，建议设置比数据库超时时长少30秒，
         // 参考MySQL wait_timeout参数（show variables like '%timeout%';）
         config.setMaxLifetime(28770000);
-
         HikariDataSource server_hikari = new HikariDataSource(config);
-//        server_hikari.setPoolName("uriel_server_hikari");
-
-
         return server_hikari;
     }
 
@@ -53,17 +48,6 @@ public class SpringConfig {
         redisTemplate.setConnectionFactory(connectionFactory);
         return redisTemplate;
     }
-
-    @Bean
-    public HashOperations hashOperations(RedisTemplate redisTemplate) {
-        return redisTemplate.opsForHash();
-    }
-
-    @Bean
-    public ValueOperations valueOperations(RedisTemplate redisTemplate) {
-        return redisTemplate.opsForValue();
-    }
-
     /**
      * 给mybatis添加插件
      *
@@ -74,10 +58,8 @@ public class SpringConfig {
     public SqlSessionFactoryBean sqlSessionFactoryBean(HikariDataSource dataSource) {
         SqlSessionFactoryBean sfb = new SqlSessionFactoryBean();
         sfb.setDataSource(dataSource);
-
         Interceptor[] plugins = {new UpdateInterceptor()};
         sfb.setPlugins(plugins);
-
         return sfb;
     }
 }
