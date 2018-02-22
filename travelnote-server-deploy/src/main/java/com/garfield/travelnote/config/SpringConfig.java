400105727465ae17091e1ebd5f1a5a160fe00818
朱士竹 <zhushizhu@zhexinit.com>
2018-02-22 11:07:26 +0800

    删除多余的配置

--- travelnote-server-deploy/src/main/java/com/garfield/travelnote/config/SpringConfig.java
+++ travelnote-server-deploy/src/main/java/com/garfield/travelnote/config/SpringConfig.java
@@ -1,6 +1,7 @@
 package com.garfield.travelnote.config;
 
 import com.garfield.travelnote.dal.util.UpdateInterceptor;
+import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 import org.apache.ibatis.plugin.Interceptor;
 import org.mybatis.spring.SqlSessionFactoryBean;
@@ -21,6 +22,30 @@ import org.springframework.data.redis.core.ValueOperations;
 @Configuration
 @MapperScan(basePackages = {"com.garfield.travelnote.dal.mapper"})
 public class SpringConfig {
+
+    @Bean
+    public HikariDataSource hikariDataSource() {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl("");
+        config.setDriverClassName("");
+        config.setUsername("");
+        config.setPassword("");
+        //连接池中允许的最大连接数。缺省值：10；推荐的公式：((core_count * 2) + effective_spindle_count)
+        config.setMaximumPoolSize(10);
+        //等待连接池分配连接的最大时长（毫秒）
+        config.setConnectionTimeout(600000);
+        //一个连接idle状态的最大时长（毫秒）
+        config.setIdleTimeout(600000);
+        //一个连接的生命时长（毫秒），超时而且没被使用则被释放（retired），缺省:30分钟，建议设置比数据库超时时长少30秒，
+        // 参考MySQL wait_timeout参数（show variables like '%timeout%';）
+        config.setMaxLifetime(28770000);
+
+        HikariDataSource server_hikari = new HikariDataSource(config);
+//        server_hikari.setPoolName("uriel_server_hikari");
+
+
+        return server_hikari;
+    }
 
     @Bean
     public RedisTemplate redisTemplate(JedisConnectionFactory connectionFactory) {
@@ -49,8 +74,10 @@ public class SpringConfig {
     public SqlSessionFactoryBean sqlSessionFactoryBean(HikariDataSource dataSource) {
         SqlSessionFactoryBean sfb = new SqlSessionFactoryBean();
         sfb.setDataSource(dataSource);
+
         Interceptor[] plugins = {new UpdateInterceptor()};
         sfb.setPlugins(plugins);
+
         return sfb;
     }
 }
