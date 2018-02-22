eac5e71504f3f607a290825ef5c86ba0fd177cb6
朱士竹 <zhushizhu@zhexinit.com>
2018-02-22 11:04:46 +0800

    删除多余的配置

---
+++ travelnote-server-common/src/main/java/com/garfield/travelnote/common/bean/ResultEnum.java
@@ -0,0 +1,48 @@
+package com.garfield.travelnote.common.bean;
+
+import com.zhexinit.ov.common.model.IResultEnum;
+
+/**
+ * Created by konie on 16-8-24.
+ */
+public enum ResultEnum implements IResultEnum {
+
+    Success(0, "ok"),
+
+    // third platform login
+    ThirdLoginError(101, "Third Login Error"),
+    //用户相关
+    registed(102, "Number Registered"),
+    errorPassword(103, "Wrong User Name or Password"),
+    illegalNickname(104, "Nickname Used"),
+    userNotExist(105, "Invalid User"),
+    illegalUserId(106, "User login has expired. Please login again"),
+    userDisabled(108, "User Disabled"),
+    phoneNumError(108,"手机号码格式错误"),
+
+    nullContent(201, "Sending information cannot be blank"),
+    goodsNotExist(202, "Product information does not exist"),
+
+    systemError(999, "System Error");
+
+    private final int code;
+    private final String message;
+
+    ResultEnum(int code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return "code: " + code + " , message: " + message;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
--- travelnote-server-deploy/src/main/java/com/garfield/travelnote/config/SpringConfig.java
+++ travelnote-server-deploy/src/main/java/com/garfield/travelnote/config/SpringConfig.java
@@ -1,9 +1,6 @@
 package com.garfield.travelnote.config;
 
-import com.garfield.travelnote.biz.config.MysqlConfig;
-import com.garfield.travelnote.biz.config.RedisDisconfConfig;
 import com.garfield.travelnote.dal.util.UpdateInterceptor;
-import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 import org.apache.ibatis.plugin.Interceptor;
 import org.mybatis.spring.SqlSessionFactoryBean;
@@ -24,44 +21,6 @@ import org.springframework.data.redis.core.ValueOperations;
 @Configuration
 @MapperScan(basePackages = {"com.garfield.travelnote.dal.mapper"})
 public class SpringConfig {
-
-    @Bean
-    public HikariDataSource hikariDataSource(MysqlConfig dbConfig) {
-        HikariConfig config = new HikariConfig();
-        config.setJdbcUrl(dbConfig.getDataSourceUrl());
-        config.setDriverClassName(dbConfig.getDataSourceDriverClassName());
-        config.setUsername(dbConfig.getDataSourceUsername());
-        config.setPassword(dbConfig.getDataSourcePassword());
-        //连接池中允许的最大连接数。缺省值：10；推荐的公式：((core_count * 2) + effective_spindle_count)
-        config.setMaximumPoolSize(10);
-        //等待连接池分配连接的最大时长（毫秒）
-        config.setConnectionTimeout(600000);
-        //一个连接idle状态的最大时长（毫秒）
-        config.setIdleTimeout(600000);
-        //一个连接的生命时长（毫秒），超时而且没被使用则被释放（retired），缺省:30分钟，建议设置比数据库超时时长少30秒，
-        // 参考MySQL wait_timeout参数（show variables like '%timeout%';）
-        config.setMaxLifetime(28770000);
-
-        HikariDataSource server_hikari = new HikariDataSource(config);
-//        server_hikari.setPoolName("uriel_server_hikari");
-
-
-        return server_hikari;
-    }
-
-    @Bean
-    public JedisConnectionFactory connectionFactory(RedisDisconfConfig redisConfig) {
-        JedisConnectionFactory factory = new JedisConnectionFactory();
-        factory.setHostName(redisConfig.getRedisHost());
-        factory.setPassword(redisConfig.getRedisPassword());
-        factory.setPort(redisConfig.getRedisPort());
-        factory.setUsePool(true);
-        factory.setTimeout(redisConfig.getRedisTimeOut());
-        factory.getPoolConfig().setMaxIdle(-1);
-        factory.getPoolConfig().setMaxTotal(-1);
-        factory.getPoolConfig().setMaxWaitMillis(60000);
-        return factory;
-    }
 
     @Bean
     public RedisTemplate redisTemplate(JedisConnectionFactory connectionFactory) {
@@ -90,10 +49,8 @@ public class SpringConfig {
     public SqlSessionFactoryBean sqlSessionFactoryBean(HikariDataSource dataSource) {
         SqlSessionFactoryBean sfb = new SqlSessionFactoryBean();
         sfb.setDataSource(dataSource);
-
         Interceptor[] plugins = {new UpdateInterceptor()};
         sfb.setPlugins(plugins);
-
         return sfb;
     }
 }
